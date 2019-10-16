31f021d71a4f1608a1b59e7812427cc3cbfc04d6
Dhruv <dhruvbandaria@hotmail.com>
2019-10-16 13:24:35 -0400

    Mover

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    private Rigidbody2D rBody;
+    public float speed=20f;
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody2D>();
+        rBody.velocity = Vector2.right * speed;
+    }
+}
